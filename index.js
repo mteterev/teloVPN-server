49ddd769da448cf7f1679ceecd57b5f871798b1a
russiancmo <russiancmo@gmail.com>
2024-02-17 01:29:30 +0300

    add ready status

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const userRouter = require('./routes/userRoute.js')
-const serverRouter = require('./routes/serverRoute.js')
-const PORT = process.env.PORT || 8080
-const app = express()
+const express = require('express');
+const userRouter = require('./routes/userRoute.js');
+const serverRouter = require('./routes/serverRoute.js');
+const PORT = process.env.PORT || 8080;
+const app = express();
 
-app.use(express.json())
-app.use('/api', userRouter, serverRouter)
+app.use(express.json());
+app.use('/api', userRouter, serverRouter);
 
-app.listen(PORT, () => console.log(`server started on port ${PORT}`))
-
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+  process.send('ready');
+});
