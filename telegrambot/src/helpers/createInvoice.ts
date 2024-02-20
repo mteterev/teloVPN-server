5a9605b441ffa9056e6bfedc7731ed92cd0dcd18
russiancmo <russiancmo@gmail.com>
2024-02-20 20:28:22 +0300

    add invoice data

--- telegrambot/src/helpers/createInvoice.ts
+++ telegrambot/src/helpers/createInvoice.ts
@@ -8,6 +8,21 @@ interface ICreateInvoice {
 }
 
 export const createInvoice = ({ ctx, payload, amount }: ICreateInvoice) => {
+  const providerData = {
+    receipt: {
+      items: [
+        { 
+          description: `VPN ${payload.split('month')[0]} месяц(ев)`,
+          quantity: 1.0,
+          amount: {
+            value: amount / 100,
+            currency: 'RUB',
+          },
+          vat_code: 1,
+        },
+      ],
+    },
+  };
   ctx.api.sendInvoice(
     ctx.from.id,
     'Оплата VPN сервера',
@@ -16,6 +31,10 @@ export const createInvoice = ({ ctx, payload, amount }: ICreateInvoice) => {
     process.env.API_KEY_PROVIDER_BOT ?? '',
     'rub',
     [{ label: 'TeloVPN', amount }],
-    { need_email: true, send_email_to_provider: true }
+    {
+      need_email: true,
+      send_email_to_provider: true,
+      provider_data: JSON.stringify(providerData),
+    }
   );
 };
