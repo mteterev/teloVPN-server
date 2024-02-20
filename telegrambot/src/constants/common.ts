e8aaf99afb75c0f9d0260bd9f4dd2391c43925a9
russiancmo <russiancmo@gmail.com>
2024-02-20 19:33:28 +0300

    fix link

--- telegrambot/src/constants/common.ts
+++ telegrambot/src/constants/common.ts
@@ -1,3 +1,3 @@
 export const adminId = '203024910';
 export const adminIds = ['104115547', '203024910'];
-export const helpUrl = `tg://user?id=${adminId}`;
+export const helpUrl = `https://t.me/mteterev`;
